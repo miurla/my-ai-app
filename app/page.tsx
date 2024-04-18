330cdba94797b71f7c305fa17d462031612a1e93
Yoshiki Miura <miurap400@gmail.com>
2024-04-18 23:27:11 +0900

    Revert "Add Chat component and refactor Page component"
    This reverts commit 137f0e8eae081d00dd910c043886a26241e17a15.

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,56 @@
-import Chat from "./chat";
+"use client";
+
+import { useState } from "react";
+import { useUIState, useActions } from "ai/rsc";
+import type { AI } from "./action";
+
+export const runtime = "edge";
 
 export default function Page() {
-  return <Chat />;
+  const [inputValue, setInputValue] = useState("");
+  const [messages, setMessages] = useUIState<typeof AI>();
+  const { submitUserMessage } = useActions<typeof AI>();
+
+  return (
+    <div>
+      {
+        // View messages in UI state
+        messages.map((message) => (
+          <div key={message.id}>{message.display}</div>
+        ))
+      }
+
+      <form
+        onSubmit={async (e) => {
+          e.preventDefault();
+
+          // Add user message to UI state
+          setMessages((currentMessages) => [
+            ...currentMessages,
+            {
+              id: Date.now(),
+              display: <div>{inputValue}</div>,
+            },
+          ]);
+
+          // Submit and get response message
+          const responseMessage = await submitUserMessage(inputValue);
+          setMessages((currentMessages) => [
+            ...currentMessages,
+            responseMessage,
+          ]);
+
+          setInputValue("");
+        }}
+      >
+        <input
+          placeholder="Send a message..."
+          value={inputValue}
+          onChange={(event) => {
+            setInputValue(event.target.value);
+          }}
+        />
+      </form>
+    </div>
+  );
 }
